2c6d62e08f345b9097d0dc9bc7b8754bef834a25
Florian Poullin <563493+floriandiud@users.noreply.github.com>
2022-05-30 11:49:42 +0200

    first commit

---
+++ src/schemas.py
@@ -0,0 +1,88 @@
+
+
+CUSTOMERS_SCHEMA = [{
+    "name": "Customer Id",
+    "type": 'id'
+}, {
+    "name": "First Name",
+    "type": 'first_name'
+}, {
+    "name": "Last Name",
+    "type": "last_name"
+}, {
+    "name": "Company",
+    "type": "company"
+}, {
+    "name": "City",
+    "type": "city",
+}, {
+    "name": "Country",
+    "type": "country",
+}, {
+    "name": "Phone 1",
+    "type": "phone"
+}, {
+    "name": "Phone 2",
+    "type": "phone"
+}, {
+    "name": "Email",
+    "type": "business_email"
+}, {
+    "name": "Subscription Date",
+    "type": "date_this_decade"
+}, {
+    "name": "Website",
+    "type": "website"
+}]
+
+PEOPLE_SCHEMA = [{
+    "name": "User Id",
+    "type": 'id'
+}, {
+    "name": "First Name",
+    "type": 'first_name'
+}, {
+    "name": "Last Name",
+    "type": "last_name"
+}, {
+    "name": "Sex",
+    "type": "sex"
+}, {
+    "name": "Email",
+    "type": "email"
+}, {
+    "name": "Phone",
+    "type": "phone"
+}, {
+    "name": "Date of birth",
+    "type": "date_of_birth"
+}, {
+    "name": "Job Title",
+    "type": "job"
+}]
+
+ORGANIZATIONS_SCHEMA = [{
+    "name": "Organization Id",
+    "type": 'id'
+}, {
+    "name": "Name",
+    "type": "company"
+}, {
+    "name": "Website",
+    "type": "website"
+}, {
+    "name": "Country",
+    "type": "country"
+}, {
+    "name": "Description",
+    "type": "company_desc"
+}, {
+    "name": "Founded",
+    "type": "year"
+}, {
+    "name": "Industry",
+    "type": "industry"
+}, {
+    "name": "Number of employees",
+    "type": "company_number_employees"
+}]
